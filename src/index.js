82cd9d5ed4a1879574d3111105f68a5099833780
Lucas Cid <lucasfelipaaa@gmail.com>
2024-06-04 12:59:22 -0300

    express-async-errors added on index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+require("express-async-errors");
 const express = require("express");
 const rotas = require("./rotas");
 const cors = require("cors");
 
 const app = express();
-
+app.use((error, request, response, next) => {
+  if(error instanceof AppError) {
+    return response.json({
+      message : error.message,
+      statusCode : error.statusCode
+    });
+  }
+  else {
+    
+  }
+  console.error(error);
+});
 app.use(express.json());
 app.use(cors());
 app.use(rotas);
